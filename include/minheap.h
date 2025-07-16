64ad9f81f88f62a003ebaf9b3a995d491a095d12
aaa <stepanovsm8@gmail.com>
2025-07-16 09:36:10 +0300

    min heap

---
+++ include/minheap.h
@@ -0,0 +1,28 @@
+#ifndef MIN_HEAP_H
+#define MIN_HEAP_H
+
+typedef struct min_heap min_heap;
+typedef struct min_heap_n min_heap_n;
+
+struct min_heap_n {
+  int v;
+  long double dist;
+};
+
+struct min_heap {
+  int size;
+  int cap;
+  int *pos; // vertex -> index
+
+  min_heap_n **arr;
+};
+
+min_heap *createMinHeap(int capacity);
+void freeMinHeap(min_heap *heap);
+int isEmpty(min_heap *heap);
+min_heap_n *extractMin(min_heap *heap);
+void decKey(min_heap *heap, int vertex, long double dist);
+int isInMinHeap(min_heap *heap, int vertex);
+void minHeapify(min_heap *h, int i);
+
+#endif
